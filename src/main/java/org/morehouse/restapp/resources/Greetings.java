0ea9fed8173510c7ce9900528502b3e348ec30b1
Morehouse Student <leronjulian@hotmail.com>
2018-11-19 18:07:52 -0600

    Created messageBundle.properties file in Greetings.java app

--- src/main/java/org/morehouse/restapp/resources/Greetings.java
+++ src/main/java/org/morehouse/restapp/resources/Greetings.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/hello/{name}/{locale}")
 public class Greetings {
 	@RequestMapping(value = "/{param}", method = RequestMethod.GET)
 	  public String printMessage(@PathVariable("param") String msg) {
